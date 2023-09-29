290545ac1107c17a3eeafee426e26d8bae890f58
Jinger <146416601+jingerororo@users.noreply.github.com>
2023-09-29 08:29:44 +0200

    loops file

---
+++ lesson3loopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lesson3loopsApp.swift
+//  lesson3loops
+//
+//  Created by Oleg Savelyev on 27.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct lesson3loopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
